06b75a9098b2e5dee14bd2518d6d7036624d4aec
W.Songsak <songsak.want@gmail.com>
2023-10-31 19:52:57 +0700

    feat: add badge

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,38 @@
+import numeral from 'numeral';
+
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
+
+function fetchDataAndSetBadge() {
+  // Fetch data here, e.g., using the fetch API
+  fetch('https://gold-api-wheat.vercel.app/api/gold-data')
+    .then((response) => response.json())
+    .then((data) => {
+      // Update the badge with the fetched data.
+      const goldPrices = data.goldPrices;
+      const goldOrnament = goldPrices?.find((d) => d.title === 'ทองรูปพรรณ');
+      if (goldOrnament?.data2) {
+        chrome.action.setBadgeText({
+          text: numeral(goldOrnament?.data2).format('0.0a', Math.floor),
+        });
+        chrome.action.setBadgeBackgroundColor({ color: '#ffdb19' });
+      }
+    })
+    .catch((error) => console.error('Error fetching data: ' + error));
+}
+
+// Set up an alarm to periodically fetch data
+chrome.alarms.create('fetchDataAlarm', {
+  delayInMinutes: 30, // Adjust this to your preferred fetch interval
+  periodInMinutes: 30,
+});
+
+// Listen for the alarm to trigger and fetch data
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === 'fetchDataAlarm') {
+    fetchDataAndSetBadge();
+  }
+});
+
+// Initial fetch and badge update on extension load
+fetchDataAndSetBadge();
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,25 +1,94 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
+import React, { useEffect, useState } from 'react';
 import './Popup.css';
+import numeral from 'numeral';
 
 const Popup = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  async function loadData() {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        'https://gold-api-wheat.vercel.app/api/gold-data'
+      );
+      const data = await response.json();
+      setData(data.goldPrices);
+    } catch (error) {
+      console.error(error);
+    }
+
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const goldBar = data?.find((d) => d.title === 'ทองคำแท่ง');
+  const goldOrnament = data?.find((d) => d.title === 'ทองรูปพรรณ');
+  const goldTodayTrend = data?.find((d) => d.title?.includes('วันนี้'));
+  const goldUpdatedAt = data?.find((d) => d.data1?.includes('เวลา'));
+
+  if (goldOrnament?.data2) {
+    chrome.action.setBadgeText({
+      text: numeral(goldOrnament?.data2).format('0.0a', Math.floor),
+    });
+    chrome.action.setBadgeBackgroundColor({ color: '#ffdb19' });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+    <div className="content">
+      {loading && !data && (
+        <div className="loading-content">
+          <p style={{ color: '#2c3e50' }}>กำลังอัพเดทข้อมูล...</p>
+          <p style={{ color: '#636e72' }}>ราคาทองตามประกาศสมาคมค้าทองคำ</p>
+        </div>
+      )}
+      {data && (
+        <table className="data-table">
+          <thead>
+            <tr>
+              <th>96.5%</th>
+              <th>รับซื้อ</th>
+              <th>ขายออก</th>
+            </tr>
+          </thead>
+          <tbody>
+            {goldBar && (
+              <tr>
+                <td className="title">{goldBar?.title}</td>
+                <td>{goldBar?.data1}</td>
+                <td>{goldBar?.data2}</td>
+              </tr>
+            )}
+
+            {goldOrnament && (
+              <tr>
+                <td className="title">{goldOrnament?.title}</td>
+                <td>{goldOrnament?.data1}</td>
+                <td>{goldOrnament?.data2}</td>
+              </tr>
+            )}
+
+            {goldTodayTrend && (
+              <tr>
+                <td className="title">{goldTodayTrend?.title}</td>
+                <td colSpan="2">{goldTodayTrend?.data2}</td>
+              </tr>
+            )}
+
+            {goldUpdatedAt && (
+              <tr>
+                <td colSpan="3">
+                  {goldUpdatedAt?.title} {goldUpdatedAt?.data1}{' '}
+                  {goldUpdatedAt?.data2}
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
